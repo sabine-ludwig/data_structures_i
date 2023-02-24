94d25ebdf77a1617b23702b1c7e6c0c05151732d
Sabine Ludwig <sabinemichelle31@gmail.com>
2023-02-24 08:30:57 -0800

    added doc strings outlining tasks

---
+++ implementation.py
@@ -0,0 +1,44 @@
+'''Task 1: Dictionary, Set and Tuple
+Given the following three scenarios, choose the best data structure 
+(between a dictionary, set, or tuple) to efficiently store the data. 
+Each scenario ties directly to one data structure. 
+Each data structure will be used only once. 
+You will need to determine which data structure is best for which scenario, 
+and then implement the data structure in Python.
+'''
+
+'''
+1. Store the months of the year as strings. 
+Determine the month in the data structure in which National Pi Day exists 
+and print that month to the console. 
+HINT: The number for Pi, when converted to an Integer, 
+is related to the stored location of the correct month.
+'''
+# tuple (months will not change)
+
+'''
+2. Store five fruits or vegetables.
+a. Add two of your favorite fruits and two of your favorite vegetables to the collection.
+b. Iterate over the collection and print each one to the console. 
+'''
+# set
+
+'''
+3. Store information about a user profile. 
+Use literal string interpolation to print the users profile information to the console. 
+The profile should consist of the following information:
+a. First Name
+b. Last Name
+c. Email Address
+d. Phone Number
+'''
+# dictionary 
+
+'''Task 2: List of Dictionaries
+Use a list to store the dictionary of your immediate family members, 
+with each index of the list storing its own dictionary. 
+Dictionary should contain the following keys:
+First name
+Last name
+Relation to you
+'''
